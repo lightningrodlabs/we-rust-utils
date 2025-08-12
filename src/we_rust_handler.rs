0e3ff455b31989e913c71d41174d08d9e88cb964
Matthias <tuttiacc@hotmail.com>
2025-08-12 15:06:29 +0200

    refactor!: handle errors in WeRustHandler, remove ZomeCallSigner struct, slim down WeRustHandler

--- src/we_rust_handler.rs
+++ src/we_rust_handler.rs
@@ -9,14 +9,19 @@ use napi::Result;
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
 
-struct WeRustHandler {
+#[napi]
+pub struct WeRustHandler {
     lair_client: LairClient,
 }
 
+#[napi]
 impl WeRustHandler {
     /// Connect to lair keystore
-    pub async fn new(keystore_url: String, passphrase: String) -> Self {
-        let connection_url_parsed = Url::parse(keystore_url.deref()).unwrap();
+    #[napi]
+    pub async fn connect(keystore_url: String, passphrase: String) -> Result<Self> {
+        let connection_url_parsed = Url::parse(keystore_url.deref()).map_err(|e| {
+            napi::Error::from_reason(format!("Failed to parse keystore connection URL: {e}"))
+        })?;
         let passphrase_locked_read = Arc::new(Mutex::new(sodoken::LockedArray::from(
             passphrase.as_bytes().to_vec(),
         )));
@@ -24,12 +29,13 @@ impl WeRustHandler {
         // TODO graceful error handling below
         let lair_client = ipc_keystore_connect(connection_url_parsed, passphrase_locked_read)
             .await
-            .unwrap();
+            .map_err(|e| napi::Error::from_reason(format!("Failed to connect to keystore: {e}")))?;
 
-        Self { lair_client }
+        Ok(Self { lair_client })
     }
 
     /// Sign a zome call
+    #[napi]
     pub async fn sign_zome_call(&self, payload: Vec<u8>, pub_key: Vec<u8>) -> Result<Vec<u8>> {
         let pub_key = AgentPubKey::from_raw_39(pub_key);
         let mut pub_key_2 = [0; 32];
@@ -39,44 +45,10 @@ impl WeRustHandler {
             .lair_client
             .sign_by_pub_key(pub_key_2.into(), None, payload.into())
             .await
-            .unwrap();
+            .map_err(|e| napi::Error::from_reason(format!("Failed to sign by pubkey: {e}")))?;
 
         let signature = Signature(*sig.0);
 
         Ok(signature.0.to_vec())
-    }
-}
-
-#[napi(js_name = "WeRustHandler")]
-pub struct JsWeRustHandler {
-    we_rust_handler: Option<WeRustHandler>,
-}
-
-#[napi]
-impl JsWeRustHandler {
-    #[napi(constructor)]
-    pub fn new() -> Self {
-        Self {
-            we_rust_handler: None,
-        }
-    }
-
-    #[napi]
-    pub async fn connect(keystore_url: String, passphrase: String) -> Self {
-        let we_rust_handler = WeRustHandler::new(keystore_url, passphrase).await;
-
-        JsWeRustHandler {
-            we_rust_handler: Some(we_rust_handler),
-        }
-    }
-
-    #[napi]
-    pub async fn sign_zome_call(&self, payload: Vec<u8>, pub_key: Vec<u8>) -> Result<Vec<u8>> {
-      self
-        .we_rust_handler
-        .as_ref()
-        .unwrap()
-        .sign_zome_call(payload, pub_key)
-        .await
     }
 }
